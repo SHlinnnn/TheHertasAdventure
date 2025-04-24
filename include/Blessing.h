8ad272b1f573cbccac105d95f81aa599cd23b323
HE,Shuailin <shlinnnn07@gmail.com>
2025-04-24 21:27:10 +0800

    Update Blessing.h

--- include/Blessing.h
+++ include/Blessing.h
@@ -1,18 +1,18 @@
 #pragma once
-#include <string>
 #include <vector>
+#include <string>
 #include "Player.h"
 
 struct Blessing {
     std::string name;
-    int hpBonus;
-    int atkBonus;
-    int spdBonus;
+    int hp;
+    int atk;
+    int spd;
     int cost;
 };
 
 class BlessingSystem {
 public:
-    static std::vector<Blessing> getShopBlessings(int currentStage);
-    static void applyBlessing(Player& player, const Blessing& blessing);
+    static std::vector<Blessing> getShopBlessings(int stage);
+    static void apply(Player& player, const Blessing& blessing);
 };
