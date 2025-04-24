b2db22d7ec1428efb26e5e20428ede2f3718c8c1
HE,Shuailin <shlinnnn07@gmail.com>
2025-04-24 19:13:38 +0800

    Update EventSystem.cpp

--- src/EventSystem.cpp
+++ src/EventSystem.cpp
@@ -1,6 +1,6 @@
 #include "EventSystem.h"
 #include <random>
-#include "Player.h"
+#include <cmath>
 
 void EventSystem::triggerRandomEvent(Player& player, int stage) {
     std::random_device rd;
@@ -8,16 +8,47 @@ void EventSystem::triggerRandomEvent(Player& player, int stage) {
     std::discrete_distribution<> prob{20, 45, 20, 10, 5};
     
     switch(prob(gen)) {
-        // ... 其他case不变
+        case NOTHING:
+            std::cout << "\n什么也没有发生...\n";
+            break;
+            
+        case STAT_BOOST: {
+            std::uniform_int_distribution<> statDist(0, 2);
+            int value = 15 + std::round(stage * 1.5);
+            switch(statDist(gen)) {
+                case 0: 
+                    player.applyBlessing(value, 0, 0);
+                    std::cout << "生命值永久增加 " << value << " 点！\n";
+                    break;
+                case 1:
+                    player.applyBlessing(0, value, 0);
+                    std::cout << "攻击力永久增加 " << value << " 点！\n";
+                    break;
+                case 2:
+                    player.applyBlessing(0, 0, value);
+                    std::cout << "速度永久增加 " << value << " 点！\n";
+                    break;
+            }
+            break;
+        }
         
-        case RESURRECTION:  // [MODIFIED] 新增专属逻辑
+        case STAT_REDUCE: {
+            int penalty = 10 + stage * 2;
+            player.applyBlessing(-penalty, -penalty/2, -penalty/2);
+            std::cout << "所有属性暂时降低！\n";
+            break;
+        }
+        
+        case ELITE_BATTLE: {
+            std::cout << "\n遭遇精英敌人！\n";
+            // 触发强化战斗逻辑
+            break;
+        }
+        
+        case RESURRECTION:
             if (!player.hasResurrection) {
                 player.hasResurrection = true;
-                std::cout << "■■■ 获得星神秘礼【再创世】！■■■\n"
-                          << "效果：死亡后可复活一次，恢复全部生命\n";
-            } else {
-                std::cout << "神秘力量已存在，转化为生命强化！\n";
-                player.applyBlessing(50, 0, 0);
+                std::cout << "\n获得星神秘礼【再创世】！\n";
             }
             break;
     }
