9c00e1a5988855f2615c74a90f5cb4ffcee2ffba
HE,Shuailin <shlinnnn07@gmail.com>
2025-05-03 15:30:12 +0800

    Update Blessing.cpp

--- src/Blessing.cpp
+++ src/Blessing.cpp
@@ -4,11 +4,11 @@ std::vector<Blessing> BlessingSystem::getShopBlessings(int Stage) {
     int priceMultiplier = 1 + Stage/5;
     
     return {
-        {"丰檀生薪", 30, 0, 0, 80 * priceMultiplier},
-        {"激变变星", 0, 25, 0, 100 * priceMultiplier},
-        {"狩星巡日", 0, 0, 40, 120 * priceMultiplier},
-        {"均衡增幅", 15, 15, 15, 150 * priceMultiplier}
-    };
+    {"Bountiful Wood", 30, 0, 0, 80 * priceMultiplier},  
+    {"Stellar Surge", 0, 25, 0, 100 * priceMultiplier},  
+    {"Celestial Hunt", 0, 0, 40, 120 * priceMultiplier},  
+    {"Harmonic Boost", 15, 15, 15, 150 * priceMultiplier}  
+};  
 }
 void BlessingSystem::apply(Player& player, const Blessing& blessing) {
     player.applyBlessing(blessing.hp, blessing.atk, blessing.spd);
