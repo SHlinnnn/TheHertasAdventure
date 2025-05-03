361935649bb4f240196ab1ae776204fe632e8669
HE,Shuailin <shlinnnn07@gmail.com>
2025-05-03 10:59:10 +0800

    Update GameSystem.h

--- include/GameSystem.h
+++ include/GameSystem.h
@@ -4,13 +4,13 @@
 
 class GameSystem {
     Player* player;
-    int currentStage = 1;
+    int currentStage;  // 从Player初始化
     void handleCombat();
     void handleShop();
     void handleEvent();
 
 public:
-    explicit GameSystem(Player* player);
+    explicit GameSystem(Player* player) : player(player), currentStage(player->currentStage) {}  // 构造函数修改
     void run();
     int getCurrentStage() const;
 private:
